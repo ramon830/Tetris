84d9f41d7113eca1d9781cd0f1b6ffaaddc5a4af
admin <ramonr830@gmail.com>
2014-05-15 12:31:43 +0300

    modify class Main

--- src/logic/Shape.java
+++ src/logic/Shape.java
@@ -3,6 +3,11 @@ package logic;
 public abstract class Shape {
     public abstract void moveShapeLeft();
     public abstract void moveShapeRight();
+    public abstract void acceleration();
     public abstract void rotate();
+    public abstract void touchLeft();
+    public abstract void touchRight();
+    public abstract void touchBottom();
+
 
 }
